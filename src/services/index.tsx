9d4033709db25f641f3349428cd7a9e93621ff99
sushmit <sushmitsarmah@gmail.com>
2024-11-22 12:04:29 +0700

    updated landing site

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,9 +2,27 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+interface ContactUsBody {
+  name?: string;
+  phone?: string;
+  email: string;
+  message?: string;
+}
+
 export const subscribe = async (email: string) => {
   try {
     const response = await axios.post(`${API_URL}/api/save-email`, { email });
+    return response.data;
+  } catch (error) {
+    console.error('Error subscribing:', error);
+    throw error;
+  }
+};
+
+export const contactus = async (body: ContactUsBody) => {
+  const url = "https://huxzfglcxl4xuwvjrh3kbbb5ri0vfyer.lambda-url.ap-southeast-1.on.aws/"
+  try {
+    const response = await axios.post(url, body);
     return response.data;
   } catch (error) {
     console.error('Error subscribing:', error);
